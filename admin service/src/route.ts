93067dee65489018c62418675f1b1efb3ec64541
Tanishq-Mathur35 <tanishqmathur975@gmail.com>
2026-07-27 20:45:10 +0530

    feat: add song and thumbnail upload functionality with admin validation

--- admin service/src/route.ts
+++ admin service/src/route.ts
@@ -1,9 +1,11 @@
 import Express from "express";
-import { addAlbum } from "./controller.js";
+import { addAlbum, addSong, addThumbnail } from "./controller.js";
 import uploadFile, { isAuth } from "./middleware.js";
 
 const router = Express.Router()
 
 router.post("/album/new", isAuth, uploadFile, addAlbum)
+router.post("/song/new", isAuth, uploadFile, addSong)
+router.post("/song/:id", isAuth, uploadFile, addThumbnail)
 
 export default router
