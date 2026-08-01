0549368e0ebe7e48d2812af2ac8adefa0bcdd6da
Tanishq-Mathur35 <tanishqmathur975@gmail.com>
2026-08-01 15:15:02 +0530

    feat: enhance caching for songs and album songs retrieval in the controller

--- song service/src/controller.ts
+++ song service/src/controller.ts
@@ -33,17 +33,47 @@ export const getAllAlbums = TryCatch(async (req, res) => {
 
 export const getAllSongs = TryCatch(async (req, res) => {
     let songs;
+    const CACHE_EXPIRY = 1800
 
-    songs = await sql`SELECT * FROM songs`
+    if (redisClient.isReady) {
+        songs = await redisClient.get("songs")
+    }
 
-    res.json(songs)
+    if (songs) {
+        console.log("Cache Hit")
+        res.json(JSON.parse(songs))
+        return
+    }
+    else {
+        console.log("Cache Miss")
+        songs = await sql`SELECT * FROM songs`
+
+        if (redisClient.isReady) {
+            await redisClient.set("songs", JSON.stringify(songs), {
+                EX: CACHE_EXPIRY
+            })
+        }
+
+        res.json(songs)
+        return
+    }
 })
 
 
 export const getAllSongsOfAlbum = TryCatch(async (req, res) => {
-    const {id} = req.params
+    const { id } = req.params
+    const CACHE_EXPIRY = 1800
 
-    let album, songs;
+    let album, songs
+
+    if (redisClient.isReady) {
+        const cachedData = await redisClient.get(`album_songs_${id}`)
+        if (cachedData) {
+            console.log("Cache Hit!")
+            res.json(JSON.parse(cachedData))
+            return
+        }
+    }
 
     album = await sql`SELECT * FROM albums WHERE id = ${id}`
 
@@ -57,6 +87,14 @@ export const getAllSongsOfAlbum = TryCatch(async (req, res) => {
     songs = await sql`SELECT * FROM songs WHERE album_id = ${id}`
 
     const response = { songs, album: album[0] }
+
+    if (redisClient.isReady) {
+        await redisClient.set(`album_songs_${id}`, JSON.stringify(response), {
+            EX: CACHE_EXPIRY
+        })
+    }
+
+    console.log("Cache Miss!")
 
     res.json(response)
 })
